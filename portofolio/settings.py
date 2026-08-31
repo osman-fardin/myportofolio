3c22a3030b0ccab5c18be94b936735b6b7124dd5
osman-fardin <osmanfardin5@gmail.com>
2026-08-31 17:00:58 +0700

    Rename Django config package

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -68,7 +68,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'myportofolio.urls'
+ROOT_URLCONF = 'portofolio.urls'
 
 TEMPLATES = [
     {
@@ -85,7 +85,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'myportofolio.wsgi.application'
+WSGI_APPLICATION = 'portofolio.wsgi.application'
 
 
 # Database
